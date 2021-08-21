c86aceda2e1261feb37fb3488082fb82063043d3
Athena84 <n.a.w.kreuk@gmail.com>
2021-08-21 16:26:43 +0200

    Combine Yr+Mo into single feature

--- DataCleaning.R
+++ DataCleaning.R
@@ -3,7 +3,7 @@
 train_data <- read.csv("./Data/train.csv", stringsAsFactors = TRUE)
 test_data <- read.csv("./Data/test.csv", stringsAsFactors = TRUE)
 
-#head(train_data)
+head(train_data)
 #head(test_data)
 #summary(train_data)
 
@@ -11,10 +11,13 @@ test_data <- read.csv("./Data/test.csv", stringsAsFactors = TRUE)
 #Check missing values as % for columns
 #colMeans(is.na(train_data)) * 100
 
-selected_cols <- c("Id", "MSSubClass", "LotArea", "BldgType", "HouseStyle", "OverallQual", "OverallCond", "YearBuilt", "YearRemodAdd", "BedroomAbvGr", "FullBath", "GrLivArea", "MoSold", "YrSold", "SaleType", "SaleCondition")
+selected_cols <- c("Id", "MSSubClass", "LotArea", "BldgType", "HouseStyle", "OverallQual", "OverallCond", "YearBuilt", "YearRemodAdd", "BedroomAbvGr", "FullBath", "GrLivArea", "SaleType", "SaleCondition")
 cleaned_train_data <- train_data[selected_cols]
 cleaned_test_data <- test_data[selected_cols]
 cleaned_train_target <- train_data["SalePrice"]
+
+#Combining month+year into timeseries
+cleaned_train_data$YrSold = train_data$YrSold + train_data$MoSold / 12
 
 #Save cleaned data set
 write.csv(cleaned_train_data, "./Data/cleaned_train.csv", row.names = FALSE)
